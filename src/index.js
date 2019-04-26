d0dbd4f3f6f0ffc7c98d8b3a4f0c4e13d7c0670c
huxiaodan <hu_xi@worksap.co.jp>
2019-04-26 18:49:01 +0800

    create front-end project

---
+++ src/index.js
@@ -0,0 +1,64 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+
+const DUMMY_DATA = [
+  {
+    senderId: "perborgen",
+    text: "who'll win?"
+  },
+  {
+    senderId: "janedoe",
+    text: "who'll win?"
+  }
+]
+
+class App extends React.Component {
+  
+  constructor() {
+    super()
+    this.state = {
+       messages: DUMMY_DATA
+    }
+  }
+  
+  render() {
+    return (
+      <div className="app">
+        <MessageList messages={this.state.messages} />
+        {/*<SendMessageForm />*/}
+     </div>
+    )
+  }
+}
+
+class MessageList extends React.Component {
+  render() {
+    return (
+      <ul className="message-list">                 
+        {this.props.messages.map(message => {
+          return (
+           <li key={message.id}>
+             <div>
+               {message.senderId}
+             </div>
+             <div>
+               {message.text}
+             </div>
+           </li>
+         )
+       })}
+     </ul>
+    )
+  }
+}
+
+
+
+// ========================================
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+);
+
