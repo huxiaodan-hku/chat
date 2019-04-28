128fca78a27e7f5506fd3d74861b16431279ab96
huxiaodan <hu_xi@worksap.co.jp>
2019-04-28 19:35:49 +0800

    develop front page1

--- src/index.js
+++ src/index.js
@@ -1,64 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import './static/index.css';
+import './MessageListArea.js'
 
-const DUMMY_DATA = [
-  {
-    senderId: "perborgen",
-    text: "who'll win?"
-  },
-  {
-    senderId: "janedoe",
-    text: "who'll win?"
-  }
-]
-
-class App extends React.Component {
-  
-  constructor() {
-    super()
-    this.state = {
-       messages: DUMMY_DATA
-    }
-  }
-  
-  render() {
-    return (
-      <div className="app">
-        <MessageList messages={this.state.messages} />
-        {/*<SendMessageForm />*/}
-     </div>
-    )
+class Main extends React.Component{
+  render(){
+    return(
+      <div>
+        <MessageListArea />
+        <div className="shopping-list">
+          <h1>Shopping List for asdfasdf</h1>
+          <ul>
+            <li>Instagram</li>
+            <li>WhatsApp</li>
+            <li>Oculus</li>
+          </ul>
+        </div>
+      </div>
+    );
   }
 }
-
-class MessageList extends React.Component {
-  render() {
-    return (
-      <ul className="message-list">                 
-        {this.props.messages.map(message => {
-          return (
-           <li key={message.id}>
-             <div>
-               {message.senderId}
-             </div>
-             <div>
-               {message.text}
-             </div>
-           </li>
-         )
-       })}
-     </ul>
-    )
-  }
-}
-
-
-
-// ========================================
 
 ReactDOM.render(
-  <App />,
+  <Main />,
   document.getElementById('root')
 );
-
